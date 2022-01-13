bf4484e391d05e12e09425e2544dd28f751b728e
Ajurna <ajurna@gmail.com>
2022-01-13 13:40:27 +0000

    fix for django-sri or static files not detecting new files. not sure which but clearing files on collectstatic fixes it.

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,5 +1,5 @@
 python manage.py migrate --settings=cbreader.settings.base
 
-python manage.py collectstatic --settings=cbreader.settings.base  --noinput
+python manage.py collectstatic --settings=cbreader.settings.base  --noinput --clear
 
 gunicorn --workers 3 --bind 0.0.0.0:8000 cbreader.wsgi:application
