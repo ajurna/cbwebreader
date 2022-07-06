cb199668fb95ec46594f541543ea36414863db6a
Ajurna <ajurna@gmail.com>
2022-07-06 08:47:32 +0100

    security fix

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+poetry export -f requirements.txt --output requirements.txt
 $version=poetry version -s
 docker build . --no-cache -t ajurna/cbwebreader -t ajurna/cbwebreader:$version
 docker push ajurna/cbwebreader --all-tags
