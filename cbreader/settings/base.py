f0bcf6df6ff9df8847c74cb70c3ef81bc82a662e
Peter Dwyer <peter.dwyer@clanwilliamhealth.com>
2022-08-31 15:16:42 +0100

    merging logger.py into base.py.

--- cbreader/settings/base.py
+++ cbreader/settings/base.py
@@ -119,7 +119,59 @@ UNRAR_TOOL = os.getenv("DJANGO_UNRAR_TOOL", None)
 
 COMIC_BOOK_VOLUME = Path(os.getenv("COMIC_BOOK_VOLUME", '/comics'))
 
-from .logger import LOGGING
+if DEBUG:
+    min_level = 'DEBUG'
+else:
+    min_level = 'INFO'
+
+min_django_level = 'INFO'
+
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,  # keep Django's default loggers
+    'formatters': {
+        # see full list of attributes here:
+        # https://docs.python.org/3/library/logging.html#logrecord-attributes
+        'verbose': {
+            'format': '%(levelname)s %(asctime)s %(module)s %(process)d %(thread)d %(message)s'
+        },
+        'simple': {
+            'format': '%(levelname)s %(message)s'
+        },
+        'timestampthread': {
+            'format': "%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s] [%(name)-20.20s]  %(message)s",
+        },
+    },
+    'handlers': {
+        'logfile': {
+            # optionally raise to INFO to not fill the log file too quickly
+            'level': min_level,  # this level or higher goes to the log file
+            'class': 'logging.handlers.RotatingFileHandler',
+            # IMPORTANT: replace with your desired logfile name!
+            'filename': os.path.join(BASE_DIR, 'djangoproject.log'),
+            'maxBytes': 50 * 10**6,  # will 50 MB do?
+            'backupCount': 3,  # keep this many extra historical files
+            'formatter': 'timestampthread'
+        },
+        'console': {
+            'level': min_level,  # this level or higher goes to the console
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {  # configure all of Django's loggers
+            'handlers': ['logfile', 'console'],
+            'level': min_django_level,  # this level or higher goes to the console
+            'propagate': False,  # don't propagate further, to avoid duplication
+        },
+        # root configuration – for all of our own apps
+        # (feel free to do separate treatment for e.g. brokenapp vs. sth else)
+        '': {
+            'handlers': ['logfile', 'console'],
+            'level': min_level,  # this level or higher goes to the console,
+        },
+    },
+}
 
 SILK_ENABLED = False
 
