875c5e75174b507b4e83937fd12117a84de16c77
Ajurna <ajurna@gmail.com>
2022-09-07 15:04:46 +0100

    Update frontend/src/components/ConfirmButton.vue
    Co-authored-by: codacy-production[bot] <61871480+codacy-production[bot]@users.noreply.github.com>

--- frontend/src/components/ConfirmButton.vue
+++ frontend/src/components/ConfirmButton.vue
@@ -2,7 +2,7 @@
   <div class="btn-group" role="group" aria-label="Basic example">
     <button type="button" class="btn" :class="'btn-'+color" v-if="!confirm" @click="confirm = !confirm">{{ label }}</button>
     <button type="button" class="btn btn-outline-success text-nowrap" v-if="confirm" @click="performAction">
-      <font-awesome-icon icon='check' class=""/>Yes
+      <font-awesome-icon icon="check" class="" />Yes
     </button>
     <button type="button" class="btn btn-outline-danger text-nowrap" v-if="confirm" @click="confirm = !confirm">
       <font-awesome-icon icon='times' class=""/>No
